84c3b52996161aa72010bc4e9f0d36dcf193e854
adil-khursheed <adilkhursheed60@gmail.com>
2023-04-15 17:05:12 +0530

    cors error

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 // Using Middlewares
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
