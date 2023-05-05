f216ccb1c1d66c981828f9fb89ea33e1ec86a7e4
adil-khursheed <adilkhursheed60@gmail.com>
2023-05-05 12:19:18 +0530

    cors error fixed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://coursebundler-one.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
